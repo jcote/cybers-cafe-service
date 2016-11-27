51b837497c55b9fe8343e7af8e3f013d25d7f17e
Jordan Cote <jordocote@gmail.com>
2016-11-26 21:44:15 -0500

    addEntity

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 
+var assets = [];
+
 var players = [];
 
 function Player (id) {
@@ -10,6 +12,48 @@ function Player (id) {
     this.z = 0;
     this.entity = null;
 }
+
+var entity = {
+    "position": [
+        0,
+        0.5,
+        0
+    ],
+    "scale": [
+        1,
+        1,
+        1
+    ],
+    "name": "Box",
+//    "parent": null, //"e8ae4254-6751-11e5-bd2a-8438356164ca",
+    "resource_id": "877bfc38-a779-11e6-8e09-22000ac481df",
+    "components": {
+        "model": {
+            "lightMapSizeMultiplier": 1,
+            "castShadows": true,
+            "castShadowsLightmap": true,
+            "lightmapped": false,
+            "materialAsset": null,
+            "receiveShadows": true,
+            "enabled": true,
+            "castShadowsLightMap": false,
+            "asset": null,
+            "lightmapSizeMultiplier": 1,
+            "type": "box",
+            "lightMapped": false,
+            "isStatic": false
+        }
+    },
+    "rotation": [
+        0,
+        0,
+        0
+    ],
+    "enabled": true,
+    "children": [],
+    "tags": []
+};
+
 
 io.sockets.on('connection', function(socket) {
     socket.on ('initialize', function () {
@@ -29,6 +73,12 @@ io.sockets.on('connection', function(socket) {
             players[data.id].z = data.z;
 
         socket.broadcast.emit ('playerMoved', data);
+    });
+
+    socket.on ('getEntity', function () {
+        var data = {};
+        data.entity = entity;
+        socket.emit('addEntity', data);
     });
 });
 
