ad3309d18dcdd92a46761ea484dc2a08c453f965
Jordan Cote <jordocote@gmail.com>
2016-12-15 17:07:50 -0500

    Serve entity+asset from datastore 2

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
+var config = require('./config');
+
+function getModel () {
+  return require('./entities/model-' + config.get('DATA_BACKEND'));
+}
 
 var players = [];
 
@@ -10,323 +15,6 @@ function Player (id) {
     this.z = 0;
     this.entity = null;
 }
-
-var entity = {
-    "position": [
-        0,
-        2.5,
-        0
-    ],
-    "scale": [
-        1,
-        1,
-        1
-    ],
-    "name": "Box",
-    "parent": "e8ae4254-6751-11e5-bd2a-8438356164ca",
-    "resource_id": "877bfc38-a779-11e6-8e09-22000ac481df",
-    "components": {
-        "model": {
-            "lightMapSizeMultiplier": 1,
-            "castShadows": true,
-            "castShadowsLightmap": true,
-            "lightmapped": false,
-            "materialAsset": 5816460,
-            "receiveShadows": true,
-            "enabled": true,
-            "castShadowsLightMap": false,
-            "asset": null,
-            "lightmapSizeMultiplier": 1,
-            "type": "box",
-            "lightMapped": false,
-            "isStatic": false
-        },
-        "rigidbody": {
-            "enabled": true,
-            "type": "dynamic",
-            "mass": 3,
-            "linearDamping": 0.5,
-            "angularDamping": 0.5,
-            "linearFactor": [
-                1,
-                1,
-                1
-            ],
-            "angularFactor": [
-                0,
-                0,
-                0
-            ],
-            "friction": 0.75,
-            "restitution": 0.5
-        },
-        "collision": {
-            "enabled": true,
-            "type": "box",
-            "halfExtents": [
-                0.5,
-                0.5,
-                0.5
-            ],
-            "radius": 0.5,
-            "axis": 1,
-            "height": 2,
-            "asset": null
-        }
-    },
-    "rotation": [
-        0,
-        0,
-        0
-    ],
-    "enabled": true,
-    "children": [],
-    "tags": []
-};
-
-
-
-var assets = {
-    "5816460": {
-        "tags": [],
-        "name": "New Material",
-        "revision": 1,
-        "preload": true,
-        "meta": null,
-        "data": {
-            "shader": "blinn",
-            "ambient": [
-                0,
-                0,
-                0
-            ],
-            "ambientTint": false,
-            "aoMap": null,
-            "aoMapVertexColor": false,
-            "aoMapChannel": "r",
-            "aoMapUv": 0,
-            "aoMapTiling": [
-                1,
-                1
-            ],
-            "aoMapOffset": [
-                0,
-                0
-            ],
-            "occludeSpecular": 1,
-            "diffuse": [
-                1,
-                1,
-                1
-            ],
-            "diffuseMap": 5816484,
-            "diffuseMapVertexColor": false,
-            "diffuseMapChannel": "rgb",
-            "diffuseMapUv": 0,
-            "diffuseMapTiling": [
-                1,
-                1
-            ],
-            "diffuseMapOffset": [
-                0,
-                0
-            ],
-            "diffuseMapTint": false,
-            "specular": [
-                0.23,
-                0.23,
-                0.23
-            ],
-            "specularMapVertexColor": false,
-            "specularMapChannel": "rgb",
-            "specularMapUv": 0,
-            "specularMap": 0,
-            "specularMapTiling": [
-                1,
-                1
-            ],
-            "specularMapOffset": [
-                0,
-                0
-            ],
-            "specularMapTint": false,
-            "specularAntialias": true,
-            "useMetalness": false,
-            "metalnessMap": 0,
-            "metalnessMapVertexColor": false,
-            "metalnessMapChannel": "r",
-            "metalnessMapUv": 0,
-            "metalnessMapTiling": [
-                1,
-                1
-            ],
-            "metalnessMapOffset": [
-                0,
-                0
-            ],
-            "metalnessMapTint": false,
-            "metalness": 1,
-            "conserveEnergy": true,
-            "shininess": 32,
-            "glossMap": 0,
-            "glossMapVertexColor": false,
-            "glossMapChannel": "r",
-            "glossMapUv": 0,
-            "glossMapTiling": [
-                1,
-                1
-            ],
-            "glossMapOffset": [
-                0,
-                0
-            ],
-            "fresnelModel": 0,
-            "fresnelFactor": 0,
-            "emissive": [
-                0,
-                0,
-                0
-            ],
-            "emissiveMap": 0,
-            "emissiveMapVertexColor": false,
-            "emissiveMapChannel": "rgb",
-            "emissiveMapUv": 0,
-            "emissiveMapTiling": [
-                1,
-                1
-            ],
-            "emissiveMapOffset": [
-                0,
-                0
-            ],
-            "emissiveMapTint": false,
-            "emissiveIntensity": 1,
-            "normalMap": 0,
-            "normalMapTiling": [
-                1,
-                1
-            ],
-            "normalMapOffset": [
-                0,
-                0
-            ],
-            "normalMapUv": 0,
-            "bumpMapFactor": 1,
-            "heightMap": 0,
-            "heightMapChannel": "r",
-            "heightMapUv": 0,
-            "heightMapTiling": [
-                1,
-                1
-            ],
-            "heightMapOffset": [
-                0,
-                0
-            ],
-            "heightMapFactor": 1,
-            "alphaTest": 0,
-            "opacity": 1,
-            "opacityMap": 0,
-            "opacityMapVertexColor": false,
-            "opacityMapChannel": "r",
-            "opacityMapUv": 0,
-            "opacityMapTiling": [
-                1,
-                1
-            ],
-            "opacityMapOffset": [
-                0,
-                0
-            ],
-            "reflectivity": 1,
-            "refraction": 0,
-            "refractionIndex": 0.6666666666666666,
-            "sphereMap": 0,
-            "cubeMap": 0,
-            "cubeMapProjection": 0,
-            "cubeMapProjectionBox": {
-                "center": [
-                    0,
-                    0,
-                    0
-                ],
-                "halfExtents": [
-                    0.5,
-                    0.5,
-                    0.5
-                ]
-            },
-            "lightMap": 5816484,
-            "lightMapVertexColor": false,
-            "lightMapChannel": "rgb",
-            "lightMapUv": 1,
-            "lightMapTiling": [
-                1,
-                1
-            ],
-            "lightMapOffset": [
-                0,
-                0
-            ],
-            "depthTest": true,
-            "depthWrite": true,
-            "cull": 1,
-            "blendType": 3,
-            "shadowSampleType": 1,
-            "useFog": true,
-            "useLighting": true,
-            "useSkybox": true,
-            "useGammaTonemap": true
-        },
-        "type": "material",
-        "file": null,
-        "region": "eu-west-1",
-        "id": "5816460"
-    },
-    "5816484": {
-        "tags": [],
-        "name": "1478204423322s.jpg",
-        "revision": 1,
-        "preload": true,
-        "meta": {
-            "compress": {
-                "alpha": false,
-                "dxt": false,
-                "pvr": false,
-                "pvrBpp": 4,
-                "etc1": false
-            },
-            "format": "jpeg",
-            "type": "TrueColor",
-            "width": 256,
-            "height": 256,
-            "alpha": false,
-            "depth": 8,
-            "srgb": true,
-            "interlaced": false
-        },
-        "data": {
-            "addressu": "repeat",
-            "addressv": "repeat",
-            "minfilter": "linear_mip_linear",
-            "magfilter": "linear",
-            "anisotropy": 1,
-            "rgbm": false,
-            "mipmaps": true
-        },
-        "type": "texture",
-        "file": {
-            "filename": "1478204423322s.jpg",
-            "hash": "0bb52b8f8aba81c88b889953eee80d65",
-            "size": 6327,
-            "variants": {},
-            "url": "files/assets/5816484/1/1478204423322s.jpg"
-        },
-        "region": "eu-west-1",
-        "id": "5816484"
-    }
-};
-
 
 io.sockets.on('connection', function(socket) {
     socket.on ('initialize', function () {
@@ -350,17 +38,34 @@ io.sockets.on('connection', function(socket) {
     });
 
     socket.on ('getEntity', function () {
-        for (id in assets) {
-            var data = {};
-            data.asset = assets[id];            
-            socket.emit('addAsset', data);
-            console.log('emit asset');
-        }
+        console.log("getEntity");
+        getModel().list('Asset', 10, null, function (err, assets, cursor) {
+            if (err) {
+              console.log("db asset list error: " + err);
+              return err;
+            }
 
-        var data = {};
-        data.entity = entity;
-        socket.emit('addEntity', data);
-        console.log('emit entity');
+            for (var key in assets) {
+                var data = {};
+                data.asset = assets[key];            
+                socket.emit('addAsset', data);
+                console.log('emit asset');
+            }
+        });
+
+        getModel().list('Entity', 10, null, function (err, entities, cursor) {
+            if (err) {
+              console.log("db entity list error: " + err);
+              return err;
+            }
+
+            for (var key in entities) {
+                var data = {};
+                data.entity = entities[key];
+                socket.emit('addEntity', data);
+                console.log('emit entity');
+            }
+        });
     });
 });
 
