b1f8f978e2e5230d498f831efd2f8b867580a82a
Jordan Cote <jordocote@gmail.com>
2016-12-31 19:30:50 -0500

    Mysql bug fix

--- entities/records-cloudsql.js
+++ entities/records-cloudsql.js
@@ -9,7 +9,7 @@ const async = require('async');
 var connection;
 
 function handleDisconnect() {
-  var connection = mysql.createConnection({
+  connection = mysql.createConnection({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     socketPath: process.env.MYSQL_SOCKET_PATH,
@@ -22,6 +22,7 @@ function handleDisconnect() {
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
     }                                     // to avoid a hot loop, and to allow our node script to
+    console.log("connected mysql");
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
   connection.on('error', function(err) {
