b66ab412257640ee23434a6a51be07310e58cc0a
Jordan Cote <jordocote@gmail.com>
2017-03-18 00:02:34 -0400

    GetAbsolutePosition rewrite

--- scripts/mathutils.js
+++ scripts/mathutils.js
@@ -61,16 +61,35 @@ function getRelativePosition(loc, pos, origin, scale) {
 }
 
 // takes a position in space anchored at origin
-// and returns position XYZ anchored at closest location XZ 
-function getAbsolutePosition (pos, scale) {
+// and returns position XYZ anchored at closest location xz 
+function getAbsolutePosition (pos, origin, scale) {
   var out = {location:[], position:[]};
-  
-  out.location[0] = Math.floor(pos[0] / scale);
-  out.location[1] = Math.floor(pos[2] / scale);
+  var halfScale = scale / 2;
 
-  out.position[0] = pos[0] % scale;
+  var locPartialX = Math.floor(Math.abs(pos[0]) / halfScale);
+  var locPartialZ = Math.floor(Math.abs(pos[2]) / halfScale);
+
+  var signPosX = locPartialX % 2 == 0 ? 1 : -1;
+  var signPosZ = locPartialZ % 2 == 0 ? 1 : -1;
+
+  var signLocX = pos[0] >= 0 ? 1 : -1;
+  var signLocZ = pos[2] >= 0 ? 1 : -1;
+
+  var floorOrCeilX = locPartialX % 2 ? Math.ceil : Math.floor;
+  var floorOrCeilZ = locPartialZ % 2 ? Math.ceil : Math.floor;
+
+  var locPartialX2 = signLocX * floorOrCeilX.call(null, locPartialX / 2);
+  var locPartialZ2 = signLocZ * floorOrCeilZ.call(null, locPartialZ / 2);
+
+  out.location[0] = locPartialX2 + origin[0];
+  out.location[1] = locPartialZ2 + origin[1];
+
+  var posPartialX = pos[0] - locPartialX2 * scale;
+  var posPartialZ = pos[2] - locPartialZ2 * scale;
+
+  out.position[0] = Math.abs(posPartialX) == 25 ? posPartialX : posPartialX % halfScale;
   out.position[1] = pos[1];
-  out.position[2] = pos[2] % scale;
+  out.position[2] = Math.abs(posPartialZ) == 25 ? posPartialZ : posPartialZ % halfScale;
 
   return out;
 }
