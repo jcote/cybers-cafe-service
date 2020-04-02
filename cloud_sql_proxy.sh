e246de6a8ca71bb952d84b7ca3e9c2d07619f064
jcote <jordocote@gmail.com>
2020-04-02 17:10:52 +0000

    setup nginx script

--- cloud_sql_proxy.sh
+++ cloud_sql_proxy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export GOOGLE_APPLICATION_CREDENTIALS="/home/latte/.google/Cybers Cafe-cc5478d309c4.json"
-/home/latte/cloud_sql_proxy -instances=cybers-cafe:us-east1:cybers-cafe=tcp:3306
+export GOOGLE_APPLICATION_CREDENTIALS="/home/nonki/.google/cybers-cafe-cb3ce5122c96.json"
+/var/www/cybers-cafe/cybers-cafe-service/cloud_sql_proxy -instances=cybers-cafe:us-east1:cybers-cafe=tcp:3306
---
+++ setup_nginx_conf.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ln -s /var/www/cybers-cafe/cybers-cafe-service/system/files/service.cybers.cafe-ssl.conf /etc/nginx/sites-available/service.cybers.cafe-ssl.conf
+ln -s /etc/nginx/sites-available/service.cybers.cafe-ssl.conf /etc/nginx/sites-enabled/service.cybers.cafe-ssl.conf 
+
